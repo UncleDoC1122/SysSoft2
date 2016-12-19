fc06eb2de56d8c3f2a2996906e4b1fc7a4695876
UncleDoC1122 <uncledoctortheone@gmail.com>
2016-12-20 00:58:31 +0300

    Finished

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,59 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbInput = new System.Windows.Forms.TextBox();
-            this.magic = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tbOutp = new System.Windows.Forms.TextBox();
+            this.magic = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.dataLexemes = new System.Windows.Forms.DataGridView();
+            this.Дучуь = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLexemes)).BeginInit();
             this.SuspendLayout();
             // 
-            // tbInput
+            // tabControl1
             // 
-            this.tbInput.Location = new System.Drawing.Point(12, 12);
-            this.tbInput.Multiline = true;
-            this.tbInput.Name = "tbInput";
-            this.tbInput.Size = new System.Drawing.Size(758, 264);
-            this.tbInput.TabIndex = 0;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(562, 427);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tbOutp);
+            this.tabPage1.Controls.Add(this.magic);
+            this.tabPage1.Controls.Add(this.tbInput);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(554, 401);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Compiler output";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataLexemes);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(554, 401);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lexeme analysis";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tbOutp
+            // 
+            this.tbOutp.Location = new System.Drawing.Point(5, 286);
+            this.tbOutp.Margin = new System.Windows.Forms.Padding(2);
+            this.tbOutp.Multiline = true;
+            this.tbOutp.Name = "tbOutp";
+            this.tbOutp.Size = new System.Drawing.Size(544, 102);
+            this.tbOutp.TabIndex = 5;
             // 
             // magic
             // 
             this.magic.ForeColor = System.Drawing.SystemColors.ControlText;
             this.magic.Image = global::SysSoft2.Properties.Resources._2;
-            this.magic.Location = new System.Drawing.Point(12, 282);
+            this.magic.Location = new System.Drawing.Point(5, 177);
+            this.magic.Margin = new System.Windows.Forms.Padding(2);
             this.magic.Name = "magic";
-            this.magic.Size = new System.Drawing.Size(758, 70);
-            this.magic.TabIndex = 1;
+            this.magic.Size = new System.Drawing.Size(544, 105);
+            this.magic.TabIndex = 4;
             this.magic.UseVisualStyleBackColor = true;
-            this.magic.Click += new System.EventHandler(this.magic_Click);
+            this.magic.Click += new System.EventHandler(this.magic_Click_1);
             // 
-            // tbOutp
+            // tbInput
             // 
-            this.tbOutp.Location = new System.Drawing.Point(12, 358);
-            this.tbOutp.Multiline = true;
-            this.tbOutp.Name = "tbOutp";
-            this.tbOutp.Size = new System.Drawing.Size(758, 185);
-            this.tbOutp.TabIndex = 2;
+            this.tbInput.Location = new System.Drawing.Point(5, 5);
+            this.tbInput.Margin = new System.Windows.Forms.Padding(2);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(544, 168);
+            this.tbInput.TabIndex = 3;
+            // 
+            // dataLexemes
+            // 
+            this.dataLexemes.AllowUserToAddRows = false;
+            this.dataLexemes.AllowUserToDeleteRows = false;
+            this.dataLexemes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataLexemes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Дучуь,
+            this.Type,
+            this.Value});
+            this.dataLexemes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataLexemes.Location = new System.Drawing.Point(3, 3);
+            this.dataLexemes.Name = "dataLexemes";
+            this.dataLexemes.ReadOnly = true;
+            this.dataLexemes.Size = new System.Drawing.Size(548, 395);
+            this.dataLexemes.TabIndex = 0;
+            // 
+            // Дучуь
+            // 
+            this.Дучуь.HeaderText = "Lexeme";
+            this.Дучуь.Name = "Дучуь";
+            this.Дучуь.ReadOnly = true;
+            this.Дучуь.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Дучуь.Width = 150;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Lexeme type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            this.Type.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Type.Width = 150;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Lexeme value";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            this.Value.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Value.Width = 150;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(782, 555);
-            this.Controls.Add(this.tbOutp);
-            this.Controls.Add(this.magic);
-            this.Controls.Add(this.tbInput);
+            this.ClientSize = new System.Drawing.Size(586, 451);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Lab 2";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataLexemes)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox tbInput;
-        private System.Windows.Forms.Button magic;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TextBox tbOutp;
+        private System.Windows.Forms.Button magic;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataLexemes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Дучуь;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,9 @@ namespace SysSoft2
             InitializeComponent();
         }
 
-        private int findIdentidier(List<Tuple<string, short>> mas, string name)
+        private int findIdentidier(Tuple<string, short>[] mas, string name)
         {
-            for (int i = 0; i < mas.Count; i ++)
+            for (int i = 0; i < mas.Length; i ++)
             {
                 if (mas[i].Item1.Equals(name))
                 {
@@ -29,10 +29,113 @@ namespace SysSoft2
             return -1;
         }
 
-        private Tuple<string, short>[] registers = new Tuple<string, short>[8];
-
-        private void magic_Click(object sender, EventArgs e)
+        private void lexemAnalyzer(string input)
         {
+            List<string> prog = new List<string>();
+            string tmp = "";
+            for (int i = 0; i < input.Length; i ++)
+            {
+                if (input[i].Equals(';'))
+                {
+                    prog.Add(tmp);
+                    tmp = "";
+                }
+                else if(!input[i].Equals('\r') && !input[i].Equals('\n'))
+                {
+                    tmp += input[i];
+                }
+            }
+
+            int keyWordCounter = 0;
+            int identifiersCounter = 0;
+            List<Tuple<int, string>> pairs = new List<Tuple<int, string>>();
+            String tmpr;
+
+            for (int i = 0; i < prog.Count; i ++)
+            {
+                List<string> tmp2 = separator(prog[i]);
+
+                for (int j = 0; j < tmp2.Count; j ++)
+                {
+                    if (tmp2[j] == "eah" || tmp2[j] == "ebh" || tmp2[j] == "ech" || tmp2[j] == "edh" || tmp2[j] == "eal" || tmp2[j] == "ebl" || tmp2[j] == "ecl" || tmp2[j] == "edl")
+                    {
+                        bool flah = false;
+                        for (int k = 0; k < pairs.Count; k ++)
+                        {
+                            if (pairs.Contains(new Tuple<int, string>(k + 1, tmp2[j])))
+                            {
+                                flah = true;
+                                tmpr = tmp2[j] + " : " + pairs[k].Item1;
+                                dataLexemes.Rows.Add(tmp2[j], "Identifier (register)", tmpr);
+                            } 
+                        }
+                        if (flah == false)
+                        {
+                            tmpr = tmp2[j] + " : " + ++identifiersCounter;
+                            pairs.Add(new Tuple<int, string>(identifiersCounter, tmp2[j]));
+                            dataLexemes.Rows.Add(tmp2[j], "Identifier (register)", tmpr);
+                        }
+                        
+                    }
+                    else if (tmp2[j] == "mov" || tmp2[j] == "add" || tmp2[j] == "pop" || tmp2[j] == "inc" || tmp2[j] == "div" || tmp2[j] == "push")
+                    {
+                        tmpr = tmp2[j] + " " + ++keyWordCounter;
+                        dataLexemes.Rows.Add(tmp2[j], "Keyword", tmpr);
+                    }
+                    else
+                    {
+                        dataLexemes.Rows.Add(tmp2[j], "Variable", tmp2[j]);
+                    }
+                }
+            }
+
+        }
+
+        private List<string> separator(String input)
+        {
+            List<string> output = new List<string>();
+            string putter = "";
+            for (int i = 0; i < input.Length; i ++)
+            {
+                if (input[i].Equals(' '))
+                {
+                    output.Add(putter);
+                    putter = "";
+                }
+                else if (input[i].Equals(';') == false)
+                {
+                    putter += input[i];
+                    
+                }
+            }
+            output.Add(putter);
+
+            return output;
+        }
+
+        private void outputting(Tuple<string, short>[] registers, Stack<short> stack, int step)
+        {
+            tbOutp.AppendText("Step " + step + "-----------------------------");
+            for (int i = 0; i < registers.Length; i ++)
+            {
+                tbOutp.AppendText(registers[i].Item1 + ": " + registers[i].Item2.ToString() + "; ");
+            }
+
+            tbOutp.AppendText("Stack:\n");
+
+            for (int i = 0; i < stack.Count; i ++)
+            {
+                tbOutp.AppendText(stack.Pop().ToString());
+            }
+            
+        }
+
+        private Tuple<string, short>[] registers = new Tuple<string, short>[8];
+        private Stack<short> stack = new Stack<short>();
+
+        private void magic_Click_1(object sender, EventArgs e)
+        {
+            tbOutp.Clear();
             registers[0] = new Tuple<string, short>("eah", 0);
             registers[1] = new Tuple<string, short>("eal", 0);
             registers[2] = new Tuple<string, short>("ebh", 0);
@@ -43,25 +146,117 @@ namespace SysSoft2
             registers[7] = new Tuple<string, short>("edl", 0);
 
             string input = tbInput.Text;
+            lexemAnalyzer(input);
             List<string> prog = new List<string>();
             string tmp = "";
 
-            for (int i = 0; i < input.Length; i ++)
+            for (int i = 0; i < input.Length; i++)
             {
-                if (input[i].Equals('\n'))
+                if (input[i].Equals(';'))
                 {
                     prog.Add(tmp);
                     tmp = "";
                 }
-                else
+                else if (!input[i].Equals('\r') && !input[i].Equals('\n'))
                 {
                     tmp += input[i];
                 }
             }
 
-            for (int i = 0; i < prog.Count; i ++)
+            for (int i = 0; i < prog.Count; i++)
             {
+                List<string> parsed = separator(prog[i]);
+                try
+                {
+                    int tmpIdent = -1;
+                    int tmpIdent2 = -1;
+                    short tmpValue = -1;
+                    switch (parsed[0])
+                    {
+                        case "mov":
+                            tmpIdent = findIdentidier(registers, parsed[1]);
+                            try
+                            {
+                                registers[tmpIdent] = new Tuple<string, short>(registers[tmpIdent].Item1, Convert.ToInt16(parsed[2]));
+                            }
+                            catch
+                            {
+                                tmpIdent2 = findIdentidier(registers, parsed[2]);
+                                registers[tmpIdent] = new Tuple<string, short>(registers[tmpIdent].Item1, registers[tmpIdent].Item2);
 
+                            }
+
+                            break;
+                        case "push":
+                            tmpIdent = findIdentidier(registers, parsed[0]);
+
+                            stack.Push(registers[tmpIdent].Item2);
+
+                            break;
+                        case "pop":
+                            tmpIdent = findIdentidier(registers, parsed[1]);
+
+                            registers[tmpIdent] = new Tuple<string, short>(registers[tmpIdent].Item1, stack.Pop());
+
+                            break;
+                        case "inc":
+                            tmpIdent = findIdentidier(registers, parsed[1]);
+
+                            tmpValue = registers[tmpIdent].Item2;
+                            tmpValue++;
+
+                            registers[tmpIdent] = new Tuple<string, short>(registers[tmpIdent].Item1, tmpValue);
+
+                            break;
+                        case "add":
+                            tmpIdent = findIdentidier(registers, parsed[1]);
+                            try
+                            {
+                                tmpValue = Convert.ToInt16(parsed[2]);
+                                tmpValue += registers[tmpIdent].Item2;
+                                registers[tmpIdent] = new Tuple<string, short>(registers[tmpIdent].Item1, tmpValue);
+                            }
+                            catch
+                            {
+                                tmpIdent2 = findIdentidier(registers, parsed[2]);
+
+                                tmpValue = registers[tmpIdent].Item2;
+                                tmpValue += registers[tmpIdent2].Item2;
+
+                                registers[tmpIdent] = new Tuple<string, short>(registers[tmpIdent].Item1, tmpValue);
+                            }
+
+                            break;
+                        case "div":
+                            tmpIdent = findIdentidier(registers, parsed[1]);
+                            try
+                            {
+                                tmpValue = registers[tmpIdent].Item2;
+                                tmpValue /= Convert.ToInt16(parsed[2]);
+                                registers[tmpIdent] = new Tuple<string, short>(registers[tmpIdent].Item1, tmpValue);
+                            }
+                            catch
+                            {
+                                tmpIdent2 = findIdentidier(registers, parsed[2]);
+
+                                tmpValue = registers[tmpIdent].Item2;
+                                tmpValue /= registers[tmpIdent2].Item2;
+
+                                registers[tmpIdent] = new Tuple<string, short>(registers[tmpIdent].Item1, tmpValue);
+                            }
+
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("COMPILATION ERROR");
+                    return;
+                }
+
+                outputting(registers, stack, i + 1);
             }
 
         }
